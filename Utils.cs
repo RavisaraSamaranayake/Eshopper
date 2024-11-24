973c8be6d833200237a8fc34a5176c18efdd5105
RavisaraSamaranayake <sadeepasamaranayake.2@gmail.com>
2024-11-24 15:05:20 +0530

    Add files via upload

---
+++ Utils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace Ecommerce
+{
+    public class Utils
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        SqlDataReader sdr;
+        DataTable dt;
+
+        public static string getConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+
+        public static bool isValidExtension(string fileName)
+        {
+            bool isValid = false;
+            string[] fileExtension = { ".jpg", ".png", ".jpeg" };
+            foreach (string file in fileExtension)
+            {
+                if (fileName.EndsWith(file, StringComparison.OrdinalIgnoreCase))
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+            return isValid;
+        }
+
+        public static string getUniqueId()
+        {
+            Guid guid = Guid.NewGuid();
+            return guid.ToString();
+        }
+
+        public static string getImageUrl(object url)
+        {
+            string url1 = string.Empty;
+            if (url == null || string.IsNullOrEmpty(url.ToString()) || url == DBNull.Value)
+            {
+                url1 = "../Images/No_image.png";
+            }
+            else
+            {
+                url1 = string.Format("../{0}", url);
+            }
+            return url1;
+        }
+    }
+}
